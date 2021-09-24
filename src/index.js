6a4a5685a2d1b80204a8324732b716486fc29818
Whoistolu <digittolu25@gmail.com>
2021-09-24 01:17:08 +0100

    Add the HTML dynamicallynusingnJS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import _ from 'lodash';
 import './style.css';
 
+const activities = [
+  {
+    description: 'Do the dishes',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Go to the movies',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Take a walk',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Read a novel',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Do the laundry',
+    completed: true,
+    index: 0,
+  },
+];
+ 
+activities.forEach((e) => {
+  const html = `<input type="checkbox">
+  <input value="${activities.description}" type="text"></input>
+  <i class="fas fa-ellipsis-v"></i>`;
+  const div = document.createElement('div');
+  div.innerHTML = html;
+});
